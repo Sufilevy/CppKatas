d21e91d52159d3c3d0fcc9f3b7eadd9f846d7780
Sufilevy <sufilevy@gmail.com>
2023-02-07 14:01:24 +0200

    🆕Kata: ReplaceWithAlphabetPosition

---
+++ ReplaceWithAlphabetPosition/Main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+std::string alphabet_position(const std::string& text) {
+	return "";
+}
+
+int main() {
+	std::cout << alphabet_position("The sunset sets at twelve o' clock.") << std::endl;
+}
