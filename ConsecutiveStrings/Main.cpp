7c56a7b0fb0b1cd73ff674c7e6cf031f58cc99b3
Sufilevy <sufilevy@gmail.com>
2023-02-07 13:42:36 +0200

    ✅Kata: ConsecutiveStrings

--- ConsecutiveStrings/Main.cpp
+++ ConsecutiveStrings/Main.cpp
@@ -10,7 +10,28 @@ public:
 };
 
 std::string LongestConsec::longestConsec(const std::vector<std::string>& strarr, int k) {
-	return "";
+	const int strarrSize = strarr.size();
+
+	if (strarrSize == 0 || k > strarrSize || k <= 0) {
+		return "";
+	}
+
+	std::string longestString;
+	int longestLength = 0;
+	for (int i = 0; i < strarrSize; i++) {
+		std::string consec = "";
+		for (int j = 0; j < k && i + j < strarrSize; j++) {
+			consec += strarr[i + j];
+		}
+
+		int length = consec.size();
+		if (length > longestLength) {
+			longestString = consec;
+			longestLength = length;
+		}
+	}
+
+	return longestString;
 }
 
 
