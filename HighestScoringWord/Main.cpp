991b11b0bca7313a3192eb375bc975738b15dde0
Sufilevy <sufilevy@gmail.com>
2023-02-07 14:42:29 +0200

    ✅Kata: HighestScoringWord

--- HighestScoringWord/Main.cpp
+++ HighestScoringWord/Main.cpp
@@ -3,7 +3,28 @@
 #include <string>
 
 std::string highestScoringWord(const std::string& str) {
-	return "magic";
+	std::string currentWord, highestScoringWord;
+	int currentScore = 0, highestScore = 0;
+	for (char c : str) {
+		if (c == ' ') {
+			if (currentScore > highestScore) {
+				highestScoringWord = currentWord;
+				highestScore = currentScore;
+			}
+			currentWord = "";
+			currentScore = 0;
+		}
+		else {
+			currentWord += c;
+			currentScore += std::tolower(c) - 96;
+		}
+	}
+
+	if (currentScore > highestScore) {
+		highestScoringWord = currentWord;
+	}
+
+	return highestScoringWord;
 }
 
 int main() {
