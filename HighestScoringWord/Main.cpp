ff84bae02f300ea9d3c77e05ed64e4ef9d3c4b9f
Sufilevy <sufilevy@gmail.com>
2023-02-07 14:33:58 +0200

    🆕Kata: HighestScoringWord

---
+++ HighestScoringWord/Main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <string>
+
+std::string highestScoringWord(const std::string& str) {
+	return "magic";
+}
+
+int main() {
+	std::cout << highestScoringWord("man i need a taxi up to ubud") << std::endl;
+}
